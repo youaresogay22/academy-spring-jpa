0c6ae12e14a5dec01c00ae75fd3c945dd241e264
신동민/NE <dongmyo@nhnent.com>
2022-05-23 07:08:23 +0900

    * `Orders` 테이블에 대한 entity 맵핑 실습 정답

--- src/main/java/com/nhnacademy/springjpa/entity/Order.java
+++ src/main/java/com/nhnacademy/springjpa/entity/Order.java
@@ -1,5 +1,18 @@
 package com.nhnacademy.springjpa.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 // TODO #1: `Orders` 테이블과 맵핑될 `Order` Entity 클래스를 작성하세요.
 /*
  * create table if not exists `Orders` (
@@ -10,5 +23,19 @@ package com.nhnacademy.springjpa.entity;
  * );
  *
  */
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "Orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "order_date")
+    private Date orderDate;
+
 }
