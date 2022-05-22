8ce01f19606346797f195988ef97cbffe049bd5f
신동민/NE <dongmyo@nhnent.com>
2022-05-23 06:47:56 +0900

    * `Items` 테이블에 대한 entity 맵핑 실습

---
+++ src/main/java/com/nhnacademy/springjpa/entity/Item.java
@@ -0,0 +1,20 @@
+package com.nhnacademy.springjpa.entity;
+
+// TODO #1: `Items` 테이블과 맵핑될 `Item` Entity 클래스를 작성하세요.
+/*
+ *
+ * create table if not exists `Items` (
+ *   `item_id` bigint not null auto_increment,
+ *   `item_name` varchar(40) not null,
+ *   `price` bigint not null,
+ *
+ *   primary key(`item_id`)
+ * );
+ *
+ */
+public class Item {
+    public Long getItemId() {
+        return null;
+    }
+
+}
