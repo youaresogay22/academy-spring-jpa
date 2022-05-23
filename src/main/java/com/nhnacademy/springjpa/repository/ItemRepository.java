cfa3aaaa6cb6a3fce1652f21a7ec88027d78a1c0
신동민/NE <dongmyo@nhnent.com>
2022-05-23 20:02:20 +0900

    * 메서드 이름으로 쿼리 생성

--- src/main/java/com/nhnacademy/springjpa/repository/ItemRepository.java
+++ src/main/java/com/nhnacademy/springjpa/repository/ItemRepository.java
@@ -1,7 +1,29 @@
 package com.nhnacademy.springjpa.repository;
 
+import com.nhnacademy.springjpa.annotation.Question;
 import com.nhnacademy.springjpa.entity.Item;
+import java.util.Collection;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    // select * from Items where item_name like '{itemName}'
+    List<Item> findByItemNameLike(String itemName);
+
+    // select item_id from Items
+    // where item_name = '{itemName}'
+    // and price = {price} limit 1
+    boolean existsByItemNameAndPrice(String itemName, Long price);
+
+    // select count(*) from Items where item_name like '%{itemName}%'
+    int countByItemNameLike(String itemName);
+
+    // delete from Items where price between {price1} and {price2}
+    void deleteByPriceBetween(long price1, long price2);
+
+    // TODO : #1 실습 - 다음 메서드의 이름을 아래 쿼리 결과가 나오도록 이름 규칙에 맞춰 수정하세요.
+    //        select * from Items where price in (...)
+    @Question
+    List<Item> changeThisMethodName(Collection<Long> prices);
+
 }
---
+++ src/test/java/com/nhnacademy/springjpa/repository/ItemRepositoryTest2.java
@@ -0,0 +1,57 @@
+package com.nhnacademy.springjpa.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.nhnacademy.springjpa.annotation.Question;
+import com.nhnacademy.springjpa.config.RootConfig;
+import com.nhnacademy.springjpa.config.WebConfig;
+import com.nhnacademy.springjpa.entity.Item;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@Transactional
+@ContextHierarchy({
+    @ContextConfiguration(classes = RootConfig.class),
+    @ContextConfiguration(classes = WebConfig.class)
+})
+public class ItemRepositoryTest2 {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    // TODO : #2 ItemRepository 인터페이스의 @Question 어노테이션이 붙은 메서드의 이름을 수정해서 아래 테스트를 통과하세요.
+    @Test
+    void test() {
+        Class<?> clazz = Arrays.stream(itemRepository.getClass().getInterfaces())
+            .filter(iface -> iface.isAssignableFrom(ItemRepository.class))
+            .findFirst()
+            .orElse(null);
+
+        assertThat(clazz).isNotNull();
+
+        Method questionedMethod = Arrays.stream(clazz.getDeclaredMethods())
+            .filter(method -> method.isAnnotationPresent(Question.class))
+            .findFirst()
+            .orElse(null);
+
+        assertThat(questionedMethod).isNotNull();
+
+        List<Item> items = ReflectionTestUtils.invokeMethod(itemRepository, questionedMethod.getName(),
+            Arrays.asList(100L, 200L));
+
+        assertThat(items).isNotEmpty()
+                         .hasSize(3);
+    }
+
+}
