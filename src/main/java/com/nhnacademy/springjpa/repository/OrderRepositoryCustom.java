0a781a1e0c9c64a448d91f12b17979ff770a7566
신동민/NE <dongmyo@nhnent.com>
2022-05-23 20:02:20 +0900

    * querydsl 실습 정답

--- src/main/java/com/nhnacademy/springjpa/repository/OrderRepositoryCustom.java
+++ src/main/java/com/nhnacademy/springjpa/repository/OrderRepositoryCustom.java
@@ -2,9 +2,11 @@ package com.nhnacademy.springjpa.repository;
 
 import com.nhnacademy.springjpa.entity.Item;
 import java.util.List;
+import org.springframework.data.repository.NoRepositoryBean;
 
 // TODO #1: 실습 - 중간 단계 interface의 객체 생성을 막기 위해
 //        custom repository interface에 붙여야하는 annotation을 작성하세요.
+@NoRepositoryBean
 public interface OrderRepositoryCustom {
     // TODO #2: custom 메서드 선언
     List<Item> getItemsHavingOrderItemQuantityGreaterThan(int quantity);
