9f033f44826cd16831759f558b02eea6714acf27
신동민/NE <dongmyo@nhnent.com>
2022-05-25 02:39:50 +0900

    * querdsl 실습

--- src/main/java/com/nhnacademy/springjpa/repository/ItemRepositoryCustom.java
+++ src/main/java/com/nhnacademy/springjpa/repository/ItemRepositoryCustom.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.repository.NoRepositoryBean;
 
-// TODO #1: custom repository interface 생성
 @NoRepositoryBean
 public interface ItemRepositoryCustom {
     List<Item> getItemsAfterOrderDate(LocalDateTime orderDate);
