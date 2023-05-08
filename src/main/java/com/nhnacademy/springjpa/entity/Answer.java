9dcbafbb57810b7d7d1710c8ea2f68d92f93d082
신동민/NE <dongmyo@nhnent.com>
2023-05-08 23:46:08 +0900

    * day1 training 실습 정답

--- src/main/java/com/nhnacademy/springjpa/entity/Answer.java
+++ src/main/java/com/nhnacademy/springjpa/entity/Answer.java
@@ -1,8 +1,34 @@
 package com.nhnacademy.springjpa.entity;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * TODO #3: `Answers` 테이블과 맵핑될 `Answer` Entity 클래스를 작성하세요.
  */
+@Getter
+@Setter
+@Entity
+@Table(name = "Answers")
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "answer_id")
+    private Long answerId;
+
+    @Column(name = "question_id")
+    private Long questionId;
+
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/com/nhnacademy/springjpa/entity/Question.java
+++ src/main/java/com/nhnacademy/springjpa/entity/Question.java
@@ -1,8 +1,33 @@
 package com.nhnacademy.springjpa.entity;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * TODO #2: `Questions` 테이블과 맵핑될 `Question` Entity 클래스를 작성하세요.
  */
+@Getter
+@Setter
+@Entity
+@Table(name = "Questions")
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Long questionId;
+
+    private String title;
+
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
 }
