fba88625188f1b5dc2aaab5844fa7de90b866aa4
신동민/NE <dongmyo@nhnent.com>
2023-05-09 17:45:55 +0900

    * DTO Projection 실습 정답

--- src/main/java/com/nhnacademy/springjpa/domain/ItemDto.java
+++ src/main/java/com/nhnacademy/springjpa/domain/ItemDto.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 // TODO #2: test case를 통과할 수 있도록 `ItemDto`를 작성하세요.
 public interface ItemDto {
+    String getItemName();
 
     List<OrderItemDto> getOrderItems();
 
 
     interface OrderItemDto {
+        int getQuantity();
 
         OrderDto getOrder();
 
