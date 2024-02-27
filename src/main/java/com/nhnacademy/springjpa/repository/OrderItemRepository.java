cf71bd18f67d0b7948af000f1e07dcdf6e69745e
youaresogay22 <youaresogayee@gmail.com>
2024-02-27 15:55:35 +0900

    repository 실습

---
+++ src/main/java/com/nhnacademy/springjpa/repository/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.springjpa.repository;
+
+import com.nhnacademy.springjpa.entity.Item;
+import com.nhnacademy.springjpa.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItem.Pk> {
+}
---
+++ src/main/java/com/nhnacademy/springjpa/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.springjpa.repository;
+
+import com.nhnacademy.springjpa.entity.Item;
+import com.nhnacademy.springjpa.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
