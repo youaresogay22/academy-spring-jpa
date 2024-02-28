e90d85edbf00a5a96f1f4ce2986875d8b3a9ad6d
youaresogay22 <youaresogayee@gmail.com>
2024-02-28 09:51:31 +0900

    @query 실습

--- src/main/java/com/nhnacademy/springjpa/repository/OrderRepository.java
+++ src/main/java/com/nhnacademy/springjpa/repository/OrderRepository.java
@@ -1,8 +1,10 @@
 package com.nhnacademy.springjpa.repository;
 
 import com.nhnacademy.springjpa.entity.Order;
+
 import java.util.Date;
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,7 +12,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByOrderDateAfter(Date orderDate);
 
     // TODO #1: `@Query`를 이용해서 위의 `findByOrderDateAfter()` 메서드와 동일한 기능을 하도록 JPQL을 작성하세요.
-    @Query("select o from Order o")
+    @Query("select o from Order o where o.orderDate > ?1 ")
     List<Order> getOrdersHavingOrderDateAfter(Date orderDate);
 
 }
