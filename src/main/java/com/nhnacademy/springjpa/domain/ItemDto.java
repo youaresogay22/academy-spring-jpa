2cd862eeb738548c9bc07ccf45904cb6945704e6
youaresogay22 <youaresogayee@gmail.com>
2024-02-28 11:17:23 +0900

    DTO 실습

--- src/main/java/com/nhnacademy/springjpa/domain/ItemDto.java
+++ src/main/java/com/nhnacademy/springjpa/domain/ItemDto.java
@@ -1,19 +1,20 @@
 package com.nhnacademy.springjpa.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 import java.util.List;
 
 // TODO #2: test case를 통과할 수 있도록 `ItemDto`를 작성하세요.
 public interface ItemDto {
+    String getItemName();
 
     List<OrderItemDto> getOrderItems();
 
-
     interface OrderItemDto {
-
         OrderDto getOrder();
 
+        Integer getQuantity();
     }
 
     interface OrderDto {
@@ -21,5 +22,4 @@ public interface ItemDto {
         Date getOrderDate();
 
     }
-
 }
