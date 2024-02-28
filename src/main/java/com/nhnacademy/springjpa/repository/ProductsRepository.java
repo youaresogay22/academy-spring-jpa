34aa3cae19a7fbadbf7a5419cae29f09ae7d4ff5
youaresogay22 <youaresogayee@gmail.com>
2024-02-28 10:12:54 +0900

    실습 전 저장

--- src/main/java/com/nhnacademy/springjpa/repository/ProductsRepository.java
+++ src/main/java/com/nhnacademy/springjpa/repository/ProductsRepository.java
@@ -3,5 +3,24 @@ package com.nhnacademy.springjpa.repository;
 import com.nhnacademy.springjpa.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductsRepository extends JpaRepository<Products, String> {
+import java.util.Optional;
+
+interface ProductsRepository extends JpaRepository<Products, String> {
+    Optional<Products> findByModelNumber(String modelNumber);
+
+    Optional<Products> findByModelName(String modelName);
+
+    Optional<Products> findByDescription(String description);
+
+    //#todo: 페이지네이션
+    Page<Products> pageAll(int page, int pageSize);
+
+//       crudRepo 에서 제공
+//    save(Products product);
+//    int update(Products product);
+//    int updateByProductId(String productId);
+//    int deleteByProductId(String productId);
+//    int countAll();
+//    int countByProductId(String productId);
+
 }
--- src/main/java/com/nhnacademy/springjpa/repository/UserPointDetailRepository.java
+++ src/main/java/com/nhnacademy/springjpa/repository/UserPointDetailRepository.java
@@ -4,5 +4,32 @@ package com.nhnacademy.springjpa.repository;
 import com.nhnacademy.springjpa.entity.userpointDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
 public interface UserPointDetailRepository extends JpaRepository<userpointDetail, String> {
-}
+    public int save(PointDetail pointDetail) ;
+
+    
+    public int deleteByPointDetailId(String pointDetailId);
+
+    
+    public int deleteByUserId(String userId) ;
+    
+    public Optional<PointDetail> findByPointDetailId(String pointDetailId) ;
+
+
+    
+    public Optional<PointDetail> findByUserId(String userId) ;
+    
+    public Page<PointDetail> pageAll(int page, int pageSize, String userId);
+
+
+    
+    public int countByUserId(String userId);
+    public int deleteDetailQuery(String userId, String sql) ;
+
+    public Optional<PointDetail> findDetailQuery(String userId, String sql) ;
