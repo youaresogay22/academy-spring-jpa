608ffb701ef7849536856b1d02a70e33811ec0c7
youaresogay22 <youaresogayee@gmail.com>
2024-02-28 15:25:42 +0900

    querydsl 실습

--- src/main/java/com/nhnacademy/springjpa/repository/OrderRepository.java
+++ src/main/java/com/nhnacademy/springjpa/repository/OrderRepository.java
@@ -4,5 +4,5 @@ import com.nhnacademy.springjpa.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // TODO : #6 실습 - 기본 repository가 custom repository를 상속하도록 수정하세요.
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 }
--- src/main/java/com/nhnacademy/springjpa/repository/OrderRepositoryImpl.java
+++ src/main/java/com/nhnacademy/springjpa/repository/OrderRepositoryImpl.java
@@ -1,31 +1,32 @@
 package com.nhnacademy.springjpa.repository;
 
-import com.nhnacademy.springjpa.entity.Item;
+import com.nhnacademy.springjpa.entity.*;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
 import java.util.List;
 
 // TODO #3: 실습 - querydsl를 이용해 custom repository를 구현하려면 상속받아야 하는 추상 클래스를 작성하세요.
-public class OrderRepositoryImpl extends Object/*???*/ {
+public class OrderRepositoryImpl extends QuerydslRepositorySupport implements OrderRepositoryCustom {
     public OrderRepositoryImpl() {
         // TODO #4: 실습 - 적절한 생성자 호출 구문을 쓰세요.
+        super(Order.class);
     }
 
     @Override
     public List<Item> getItemsHavingOrderItemQuantityGreaterThan(int quantity) {
-        return null;
         // TODO : #5 3번 항목까지 모두 완료하였으면 위의 return null 구문을 삭제하고 아래 주석을 해제하세요.
-        /*
+
         QOrder order = QOrder.order;
         QOrderItem orderItem = QOrderItem.orderItem;
         QItem item = QItem.item;
 
-        return from(order)
-                .innerJoin(order.orderItems, orderItem)
+        return from(orderItem)
+                .innerJoin(orderItem.order, order)
                 .innerJoin(orderItem.item, item)
                 .where(orderItem.quantity.gt(quantity))
                 .select(item)
                 .distinct()
                 .fetch();
-        */
     }
 
 }
