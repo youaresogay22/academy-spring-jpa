276377b3360c51154e4fb90dba006e4b1fa55086
youaresogay22 <youaresogayee@gmail.com>
2024-02-28 15:45:52 +0900

    N+1 - jpql 실습

--- src/main/java/com/nhnacademy/springjpa/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/springjpa/repository/MemberRepository.java
@@ -3,7 +3,9 @@ package com.nhnacademy.springjpa.repository;
 import com.nhnacademy.springjpa.domain.MemberDto;
 import com.nhnacademy.springjpa.domain.MemberNameOnly;
 import com.nhnacademy.springjpa.entity.Member;
+
 import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,7 +19,7 @@ public interface MemberRepository extends JpaRepository<Member, String> {
     Page<MemberNameOnly> getAllBy(Pageable pageable);
 
     // TODO : use join fetch
-    @Query("select m from Member m")
+    @Query("select m from Member m inner join fetch m.locker  inner join fetch m.memberDetails")
     List<Member> getMembersWithAssociation();
 
 }
