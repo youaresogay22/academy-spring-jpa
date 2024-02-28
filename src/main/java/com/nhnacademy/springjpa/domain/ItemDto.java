9b8bc7ceee4ba4d6b98b3ef142afc5f67a1de7b5
youaresogay22 <youaresogayee@gmail.com>
2024-02-28 13:16:22 +0900

    json order 추가

--- src/main/java/com/nhnacademy/springjpa/domain/ItemDto.java
+++ src/main/java/com/nhnacademy/springjpa/domain/ItemDto.java
@@ -1,11 +1,13 @@
 package com.nhnacademy.springjpa.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.util.Date;
 import java.util.List;
 
 // TODO #2: test case를 통과할 수 있도록 `ItemDto`를 작성하세요.
+@JsonPropertyOrder({"itemName", "orderItems", "order", "orderDate", "quantity"})
 public interface ItemDto {
     String getItemName();
 
