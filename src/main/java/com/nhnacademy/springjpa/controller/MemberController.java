f876ba5cb3ca34b6d1ed2b2a06a53404bbfdf86e
youaresogay22 <youaresogayee@gmail.com>
2024-02-28 13:49:11 +0900

    pageable 실습

--- src/main/java/com/nhnacademy/springjpa/controller/MemberController.java
+++ src/main/java/com/nhnacademy/springjpa/controller/MemberController.java
@@ -1,16 +1,28 @@
 package com.nhnacademy.springjpa.controller;
 
 import com.nhnacademy.springjpa.domain.MemberNameOnly;
+
 import java.util.List;
+
+import com.nhnacademy.springjpa.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
     // TODO #5: `GET /members?page=0&size=3` 와 같은 요청을 처리하도록 아래 코드를 완성하세요.
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @GetMapping("/members")
-    public List<MemberNameOnly> getMembers() {
-        return null;
+    public List<MemberNameOnly> getMembers(Pageable pageable) {
+        return memberRepository.getAllBy(pageable).getContent();
     }
 
 }
--- src/main/java/com/nhnacademy/springjpa/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/springjpa/repository/MemberRepository.java
@@ -1,14 +1,21 @@
 package com.nhnacademy.springjpa.repository;
 
+import com.nhnacademy.springjpa.domain.ItemDto;
 import com.nhnacademy.springjpa.domain.MemberDto;
 import com.nhnacademy.springjpa.domain.MemberNameOnly;
 import com.nhnacademy.springjpa.entity.Member;
+
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
     MemberNameOnly queryById(String id);
 
     List<MemberDto> findByUserName(String userName);
+
+    Page<MemberNameOnly> getAllBy(Pageable pageable);
 
 }
